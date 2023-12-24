dea2f3848352cf09642b6d0b3b92ab2ed572fd08
Eliana Ughetto <elianaughetto10@gmail.com>
2023-12-24 17:39:07 -0300

    added the home view and home routes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,42 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home Deco</title>
-</head>
-<body>
-    <header>
+@extends('layouts.main')
 
-    </header>
-</body>
-</html>
+@section('title', 'Home')
+
+@section('main')
+    <div class="banner">
+        <div class="button-container">
+            <button class="banner-button">Check new collection</button>
+        </div>
+    </div>
+    <div class='info-container'>
+        <div class="container">
+            <h1 class="font-display">We create interior products by today's standars</h1>
+            <br />
+            <div>
+                <p class="text-lightGray">Furnne is a brand that creates furniture of the highest quality with natural and
+                    ecological materials. We
+                    have been operating on the market for 10 years, our products are used by miltions of people around the
+                    world
+                    who are satistied with our products</p>
+            </div>
+            <div class="arrow-container">
+                <a href="about" class="black-text">About us <i class="fa-solid fa-arrow-right" style="color: #000000;"></i>
+                </a>
+            </div>
+        </div>
+        <div class="categories-container">
+            <div class="categories">
+                <a href='#' id="kitchen" class="categories-link">/kitchen</a>
+                <a href='#' id="living" class="categories-link">/living room</a>
+                <a href='#' id="bedroom" class="categories-link">/bedroom</a>
+                <a href='#' id="dining" class="categories-link">/dining</a>
+                <a href='#' id="bathroom" class="categories-link">/bathroom</a>
+            </div>
+            <div></div>
+        </div>
+        <div id="image-container">
+        </div>
+    </div>
+    </div>
+
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ url('css/app.css') }}">
+    <link rel="icon" href="{{ url('assets/logo.svg') }}" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+        crossorigin="anonymous" />
+
+    <title>@yield('title') :: Home Deco</title>
+
+</head>
+
+<body>
+    <header>
+        <nav class="navbar">
+            <div class="logo-container">
+                <a class="logo-link" href="{{ route('home') }}">
+                    <img src="{{ url('assets/full-logo.svg') }}" /></a>
+            </div>
+            <div class="links-container">
+                <a class="links-link" href="{{ route('home') }}">HOME</a>
+                <a class="links-link" href="{{ route('home') }}">ABOUT</a>
+                <a class="links-link" href="{{ route('home') }}">SHOP</a>
+                <a class="links-link" href="{{ route('home') }}">COLLECTION</a>
+                <a class="links-link" href="{{ route('home') }}">BLOG</a>
+            </div>
+            <div class="cart-container">
+                <a class="cart-link" href="{{ route('home') }}"><i class="fas fa-shopping-cart"
+                        style="color: #ffffff;"></i></a>
+
+            </div>
+        </nav>
+    </header>
+    <main>
+        @yield('main')
+    </main>
+    <footer class="footer">
+        <div class="footer-div">
+            <h5 class="">Diseño y Programación Web</h5>
+            <p>
+                <a class="">Profesor: Santiago Gallino</a>
+            </p>
+            <p>
+                <a class="">Portales y Comercio Electrónico</a>
+            </p>
+            <p>
+                <a class="">4 Cuatrimestre</a>
+            </p>
+        </div>
+        <div class="footer-div">
+            <h5 class="">Davinci</h5>
+            <p>
+                <a class="">Turno Tarde</a>
+            </p>
+            <p>
+                <a class="">Comisión A</a>
+            </p>
+            <p>
+                <a class="">Final</a>
+            </p>
+        </div>
+        <div class="footer-div">
+            <h5 class="">Alumna</h5>
+            <p>
+                <a class="">Eliana Ughetto</a>
+            </p>
+            <p>
+                <a class="">22 años</a>
+            </p>
+        </div>
+    </footer>
+</body>
+<script src="{{ url('js/app.js') }}"></script>
+</html>
