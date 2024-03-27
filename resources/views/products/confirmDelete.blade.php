e7fc09adeb25a63b290a10f5cc6ddf5d3d4d63b0
Eliana Ughetto <elianaughetto10@gmail.com>
2024-03-27 20:59:40 +0100

    made a file to be reusable

--- resources/views/products/confirmDelete.blade.php
+++ resources/views/products/confirmDelete.blade.php
@@ -9,32 +9,21 @@
     <div class="confirm-delete-div">
         <div class='info-container'>
             <h1>Are you sure you want to <span class="text-red">delete</span> this product?</h1>
-            <div class="new-products">
-                <div class="product">
-                    <div class="img-container">
-                        <img src="{{ url('assets/art-lamp.png') }}" alt="white art lamp">
-                    </div>
-                </div>
-                <div class="product-details-container">
-                    <h2>{{ $product->name }}</h2>
-                    <div class="product-details">
-                        <p class="bold-text">$ {{ $product->price }}</p>
-                        <br>
-                        <p>{{ $product->description }}</p>
-                    </div>
-                    <div class="buttons-container">
-                        <div class="base-button-container">
-                            <button class="base-button">Cancel</button>
-                        </div>
-                        <form action="{{ route('products.processDelete', ['id' => $product->id]) }}" method="post">
-                            @csrf
-                            <div class="base-button-container">
-                                <button type="submit" class="delete-button">Delete</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
 
+            <div class="border-red">
+                <x-product-data :product="$product" />
+            </div>
+
+            <div class="buttons-container">
+                <div class="base-button-container">
+                    <a class="base-button" href="{{ route('products') }}">Cancel</a>
+                </div>
+                <form action="{{ route('products.processDelete', ['id' => $product->id]) }}" method="post">
+                    @csrf
+                    <div class="base-button-container">
+                        <button type="submit" class="base-button delete-button">Delete</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
